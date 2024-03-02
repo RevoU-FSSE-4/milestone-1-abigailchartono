431ae8e82e3e93b700bf5b2203c0a3438ccd1849
Abigail Cherdy Hartono <abigailchartono@gmail.com>
2024-03-02 23:27:38 +0800

    first commit milestone assignment

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    console.log("Hamburger clicked"); // Check if this message appears in the console
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+function dark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    if (element.classList.contains("dark-mode")) {
+   //if class was added to body
+        localStorage.setItem("background", "dark-mode");
+   //save information in "background" localStorage variable, use dark-mode class name
+    } else {
+        localStorage.setItem("background", "");
+  //if class was removed set background" localStorage variable to null
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+//when loading document
+    var background = localStorage.getItem("background");
+  // get localStorage var background
+    if (background) {
+  // if its not null and empty
+        document.body.className += background;
+  //add class to body
+    }
+});
